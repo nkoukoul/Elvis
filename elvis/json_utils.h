df0b19f3f473387b97bff30ad9dd8d5c6325a46e
Nikos Koukoulas <koukoulas@intralot.com>
2021-07-16 15:07:18 +0300

    New validation method for json, plus some guards in json parsing

--- elvis/json_utils.h
+++ elvis/json_utils.h
@@ -10,47 +10,77 @@
 #define JSON_UTILS_H
 
 #include <unordered_map>
+#include <unordered_set>
 #include <string>
 #include <stack>
 #include <list>
 #include <memory>
 
+class validate_strategy
+{
+public:
+	validate_strategy() = default;
+	virtual bool validate(std::string const& input) const = 0;
+};
+
+class elvis_validate_strategy : public validate_strategy
+{
+public:
+	bool validate(std::string const& input) const override;
+};
+
 class deserialize_strategy
 {
 public:
-  deserialize_strategy() = default;
-  virtual std::list<std::unordered_map<std::string, std::string>> deserialize(std::string && input) const = 0;
+	deserialize_strategy() = default;
+	virtual std::list<std::unordered_map<std::string, std::string>> deserialize(std::string&& input) const = 0;
 };
 
-class nkou_deserialize_strategy : public deserialize_strategy
+class elvis_deserialize_strategy : public deserialize_strategy
 {
 public:
-  std::list<std::unordered_map<std::string, std::string>> deserialize(std::string && input) const override;
+	std::list<std::unordered_map<std::string, std::string>> deserialize(std::string&& input) const override;
 };
 
-class i_json_util_context{
+class i_json_util_context
+{
 public:
-  i_json_util_context() = default;
-  
-  std::list<std::unordered_map<std::string, std::string>> do_deserialize(std::string && input) const {
-    return ds_->deserialize(std::move(input));
-  }
-  
-  void set_deserialize_strategy(std::unique_ptr<deserialize_strategy> ds){
-    this->ds_ = std::move(ds);
-  }
+	i_json_util_context() = default;
+
+	bool do_validate(std::string const& input) const
+	{
+		return vs_->validate(input);
+	}
+
+	void set_validate_strategy(std::unique_ptr<validate_strategy> vs)
+	{
+		this->vs_ = std::move(vs);
+	}
+
+	std::list<std::unordered_map<std::string, std::string>> do_deserialize(std::string&& input) const
+	{
+		if (!vs_->validate(input)) return {};
+		return ds_->deserialize(std::move(input));
+	}
+
+	void set_deserialize_strategy(std::unique_ptr<deserialize_strategy> ds)
+	{
+		this->ds_ = std::move(ds);
+	}
 
 protected:
-  std::unique_ptr<deserialize_strategy> ds_;
+	std::unique_ptr<deserialize_strategy> ds_;
+	std::unique_ptr<validate_strategy> vs_;
 };
 
-class json_util_context : public i_json_util_context{
+class json_util_context : public i_json_util_context
+{
 public:
-  json_util_context(){
-    this->ds_ = std::make_unique<nkou_deserialize_strategy>();//default for now
-  }  
+	json_util_context()
+	{
+		set_deserialize_strategy(std::make_unique<elvis_deserialize_strategy>());//default for now
+		set_validate_strategy(std::make_unique<elvis_validate_strategy>());//default for now
+	}
 };
-
-
 
 #endif //JSON_UTILS_H
--- samples/example_app/my_controllers.cc
+++ samples/example_app/my_controllers.cc
@@ -1,70 +1,75 @@
 #include "my_controllers.h"
 
 //route is /file/{filename}
-void file_get_controller::do_stuff(std::unordered_map<std::string, std::string> &deserialized_input_data, app *ac)
+void file_get_controller::do_stuff(std::unordered_map<std::string, std::string>& deserialized_input_data, app* ac)
 {
-  //check for file existense should be added
-  std::size_t index = deserialized_input_data["url"].find_last_of("/");
-  std::string filename = deserialized_input_data["url"].substr(index + 1);
-  auto fm = std::make_unique<file_model>();
-  fm->filename_.set(filename);
-  fm->retrieve_model(ac);
-  //fm->repr();
-  auto cache = ac->cm_->access_cache<t_cache<std::string, std::string>>();
-  std::string controller_data = cache->operator[](filename);
-  if (controller_data.empty())
-  {
-    controller_data = read_from_file("", filename);
-    cache->insert(std::make_pair(filename, controller_data));
-  }
-  deserialized_input_data["controller_data"] = controller_data;
+	//check for file existense should be added
+	std::size_t index = deserialized_input_data["url"].find_last_of("/");
+	std::string filename = deserialized_input_data["url"].substr(index + 1);
+	auto fm = std::make_unique<file_model>();
+	fm->filename_.set(filename);
+	fm->retrieve_model(ac);
+	//fm->repr();
+	auto cache = ac->cm_->access_cache<t_cache<std::string, std::string>>();
+	std::string controller_data = cache->operator[](filename);
+	if (controller_data.empty())
+	{
+		controller_data = read_from_file("", filename);
+		cache->insert(std::make_pair(filename, controller_data));
+	}
+	deserialized_input_data["controller_data"] = controller_data;
 }
 
 // route is /file body is {"filename": "test.txt",  "md5": "5f7f11f4b89befa92c9451ffa5c81184"}
 // used to refresh or add to cache content
-void file_post_controller::do_stuff(std::unordered_map<std::string, std::string> &deserialized_input_data, app *ac)
+void file_post_controller::do_stuff(std::unordered_map<std::string, std::string>& deserialized_input_data, app* ac)
 {
-  // this is json data so further deserialization is needed
-  auto input = ac->juc_->do_deserialize(std::move(deserialized_input_data["data"])).front();
-  //eg input data is {"filename": "test.txt",  "md5sum_": "5f7f11f4b89befa92c9451ffa5c81184"}
-  auto fm = std::make_unique<file_model>(input["filename"], input["md5sum"]);
-  //fm->repr();
-  fm->insert_model(ac);
-  auto cache = ac->cm_->access_cache<t_cache<std::string, std::string>>();
-  cache->insert(std::make_pair(fm->filename_.get(),
-                               std::move(read_from_file("", fm->filename_.get()))));
-  cache->state();
+	// this is json data so further deserialization is needed
+	auto input = ac->juc_->do_deserialize(std::move(deserialized_input_data["data"])).front();
+	if (input.empty())
+	{
+		deserialized_input_data["status"] = "400 Bad Request";
+		return;
+	}
+	//eg input data is {"filename": "test.txt",  "md5sum_": "5f7f11f4b89befa92c9451ffa5c81184"}
+	auto fm = std::make_unique<file_model>(input["filename"], input["md5sum"]);
+	//fm->repr();
+	fm->insert_model(ac);
+	auto cache = ac->cm_->access_cache<t_cache<std::string, std::string>>();
+	cache->insert(std::make_pair(fm->filename_.get(),
+		std::move(read_from_file("", fm->filename_.get()))));
+	cache->state();
 }
 
 // http request is used to trigger a broadcast to all the ws clients
-void trigger_post_controller::do_stuff(std::unordered_map<std::string, std::string> &deserialized_input_data, app *ac)
+void trigger_post_controller::do_stuff(std::unordered_map<std::string, std::string>& deserialized_input_data, app* ac)
 {
-  // this is json data so further deserialization is needed
-  std::unordered_map<std::string, std::string> input_args =
-      {{"data", read_from_file("", "index.html")}, {"Connection", "open"}};
-  for (auto fd : ac->broadcast_fd_list)
-  {
+	// this is json data so further deserialization is needed
+	std::unordered_map<std::string, std::string> input_args =
+	{ {"data", read_from_file("", "index.html")}, {"Connection", "open"} };
+	for (auto fd : ac->broadcast_fd_list)
+	{
 
-    // executor->produce_event<std::function<void()>>(
-    // std::move(
-    //     std::bind(
-    //         &i_response_context::do_create_response,
-    //         ac->ws_ioc_->res_.get(),
-    //         fd_pair.first,
-    //         input_args,
-    //         executor)));
-  }
+		// executor->produce_event<std::function<void()>>(
+		// std::move(
+		//     std::bind(
+		//         &i_response_context::do_create_response,
+		//         ac->ws_ioc_->res_.get(),
+		//         fd_pair.first,
+		//         input_args,
+		//         executor)));
+	}
 }
 
-void user_post_controller::do_stuff(std::unordered_map<std::string, std::string> &deserialized_input_data, app *ac)
+void user_post_controller::do_stuff(std::unordered_map<std::string, std::string>& deserialized_input_data, app* ac)
 {
-  auto input = ac->juc_->do_deserialize(std::move(deserialized_input_data["data"])).front();
-  //eg input data is {"username": "test",  "password": "test"}
-  auto user = std::make_unique<user_model>();
-  user->username_.set(input["username"]);
-  user->retrieve_model(ac);
-  if (verify_password_hash(input["password"], user->password_.get()))
-  {
-    deserialized_input_data["controller_data"] = jwt_sign(user->username_.get());
-  }
+	auto input = ac->juc_->do_deserialize(std::move(deserialized_input_data["data"])).front();
+	//eg input data is {"username": "test",  "password": "test"}
+	auto user = std::make_unique<user_model>();
+	user->username_.set(input["username"]);
+	user->retrieve_model(ac);
+	if (verify_password_hash(input["password"], user->password_.get()))
+	{
+		deserialized_input_data["controller_data"] = jwt_sign(user->username_.get());
+	}
 }
